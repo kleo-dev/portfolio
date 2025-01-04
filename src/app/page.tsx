07ad4cbc426a11d07dba9080e8654eb9ee434649
Leo dev <klesti.selimay@icloud.com>
2025-01-04 19:26:43 +0100

    small mobile fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,6 @@ export default function App() {
           </div>
         </div>
       </div>
-      <footer className="px-8 md:px-10 lg:px-15 xl:px-24 text-red-400 prose absolute bottom-0 py-4"><p className="m-0 text-sm"><a className="text-red hover:text-red-400 duration-300 underline decoration-dotted underline-offset-4" target="_blank" rel="noreferrer" href="https://opensource.org/license/apache-2-0">Apache-2.0</a> © <a className="text-red hover:text-red-400 duration-300 underline decoration-dotted underline-offset-4" target="_blank" rel="noreferrer" href="https://github.com/kleo-dev">Kleo</a></p></footer>
     </div>
   );
 }
--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -7,10 +7,9 @@ export default function Pricing() {
         <div>
             <Background/>
             <div className="content-center w-full lg:mx-auto pt-16 lg:pt-0 relative z-20 pb-16 md:pb-0">
-            <div className="w-full max-w-screen-lg mx-auto">
-                <div className="px-8 md:px-10 lg:px-15 xl:px-24"></div>
+                <div className="w-full max-w-screen-lg mx-auto px-4 md:px-10">
                     <h1 className="pt-10 w-max mx-auto text-red-400">Choose a plan</h1>
-                    <div className="px-24 pt-10 h-full grid grid-cols-1 lg:grid-cols-2 grid-rows-2 gap-8">
+                    <div className="pt-10 h-full grid grid-cols-1 lg:grid-cols-2 grid-rows-2 gap-8">
                         <div className="h-full rounded-xl bg-transparent-ish border border-red p-9">
                             <FontAwesomeIcon className="w-max mx-auto text-red-600" icon={faCode} width={35}/>
                             <h1 className="w-max mx-auto text-red-400">Standard</h1>
@@ -77,7 +76,6 @@ export default function Pricing() {
                     </div>
                 </div>
             </div>
-            <footer className="px-8 md:px-10 lg:px-15 xl:px-24 text-red-400 prose absolute bottom-0 py-4"><p className="m-0 text-sm"><a className="text-red hover:text-red-400 duration-300 underline decoration-dotted underline-offset-4" target="_blank" rel="noreferrer" href="https://opensource.org/license/apache-2-0">Apache-2.0</a> © <a className="text-red hover:text-red-400 duration-300 underline decoration-dotted underline-offset-4" target="_blank" rel="noreferrer" href="https://github.com/kleo-dev">Kleo</a></p></footer>
         </div>
     )
 }
